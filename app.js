7f790dfe194bbda8b56ebde3b29cb0d4df0fb010
Deogracious1 <mikeldeogratius78@gmail.com>
2026-07-25 12:03:56 +0300

    Fix sqlite3 for Render

--- app.js
+++ app.js
@@ -1304,6 +1304,8 @@ app.get("/forgot-password", (req, res) => {
     res.render("forgot-password");
 });
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
